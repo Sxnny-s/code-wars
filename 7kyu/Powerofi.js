848559d74c3a926a2dfd9e3fb0621ab644ce299d
sunny <ethsok@protonmail.com>
2024-11-05 13:39:01 -0500

    8 kyu

---
+++ 7kyu/Powerofi.js
@@ -0,0 +1,44 @@
+// // i is the imaginary unit, it is defined by 
+// i
+// ²
+// =
+// −
+// 1
+// i²=−1, therefore it is a solution to 
+// x
+// ²
+// +
+// 1
+// =
+// 0
+// x²+1=0.
+
+// Your Task
+// Complete the function pofi that returns 
+// i
+// i to the power of a given non-negative integer in its simplest form, as a string (answer may contain 
+// i
+// i).
+
+
+
+
+
+
+//   const { assert } = require('chai');
+
+// describe('Basic Tests', function(){
+//   it('powers 0 through 10', function(){
+//     assert.strictEqual(pofi(0),'1');
+//     assert.strictEqual(pofi(1),'i');
+//     assert.strictEqual(pofi(2),'-1');
+//     assert.strictEqual(pofi(3),'-i');
+//     assert.strictEqual(pofi(4),'1');
+//     assert.strictEqual(pofi(5),'i');
+//     assert.strictEqual(pofi(6),'-1');
+//     assert.strictEqual(pofi(7),'-i');  
+//     assert.strictEqual(pofi(8),'1');
+//     assert.strictEqual(pofi(9),'i');
+//     assert.strictEqual(pofi(10),'-1');
+//   });
+// });
