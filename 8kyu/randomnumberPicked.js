9b2e5f6178b225fec63184e5c52ea5d03c0ce302
sunny <ethsok@protonmail.com>
2024-11-06 16:07:19 -0500

    fun rng

---
+++ 8kyu/randomnumberPicked.js
@@ -0,0 +1,12 @@
+function RNG(){
+ return Math.floor(Math.random() * 22) + 1;
+}
+
+
+// function RNG1(){
+//     return Math.floor(Math.random() * 3) + 1;
+// }
+
+
+// console.log(RNG1())
+console.log(RNG())
