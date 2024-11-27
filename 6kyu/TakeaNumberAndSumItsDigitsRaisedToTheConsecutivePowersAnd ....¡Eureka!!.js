57f094e1ca607a241c28d09700fa08c4b3492ca3
Sxnny-s <ethsok@protonmail.com>
2024-11-27 12:45:31 -0500

    6kyu

--- 6kyu/TakeaNumberAndSumItsDigitsRaisedToTheConsecutivePowersAnd ....¡Eureka!!.js
+++ 6kyu/TakeaNumberAndSumItsDigitsRaisedToTheConsecutivePowersAnd ....¡Eureka!!.js
@@ -14,3 +14,40 @@
 
 // 90, 100 --> []
 
+function sumDigPow(a, b) {
+    // Your code here
+    let answer = []
+    let nums = []
+    for (let i = a; i <= b; i++) {
+        
+        nums.push(i)
+    }
+
+    nums.forEach((e,i,arr) => {
+        if(e <= 9){
+            answer.push(e)
+        }else{
+            let sum = 0
+            let num = e
+          let array = e.toString().split('') 
+
+          array.forEach((e,i,arr)=>{
+            sum += Number(e)**(i+1)
+          })
+
+          if(sum === num){
+            
+            answer.push(sum)
+          }
+            }
+            
+            
+        
+    })
+
+
+    return answer
+
+  }
+  
+  console.log(sumDigPow(10,150))
